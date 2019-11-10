38603778d40e37912672c0b97637fe808a589d2b
supermuesli <art.lal@hotmail.com>
2019-11-10 05:23:55 +0100

    working example

--- main.go
+++ main.go
@@ -262,11 +262,12 @@ layout(local_size_x = 1, local_size_y = 1) in;
 layout(rgba32f, binding = 0) uniform image2D img_output;
 
 void main() {
-	// base pixel color for image
-	vec4 pixel = vec4(1.0, 0.0, 0.0, 1.0);
 	// get index in global work group i.e x,y position
 	ivec2 pixel_coords = ivec2(gl_GlobalInvocationID.xy);
 	
+	// base pixel color for image
+	vec4 pixel = vec4(float(pixel_coords[0])/800, 0.0, 0.0, 1.0);
+
 	//
 	// interesting stuff happens here later
 	//
