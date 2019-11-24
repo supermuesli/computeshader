2ca7f7b5c0def72c24d77da0032c4087f10ea5fd
supermuesli <art.lal@hotmail.com>
2019-11-24 17:52:16 +0100

    decoupled shaderutils package

--- cmd/computeShader/main.go
+++ cmd/computeShader/main.go
@@ -5,17 +5,18 @@ import (
 	_ "image/png"
 	"log"
 	"runtime"
-	"strings"
 	"github.com/go-gl/gl/v4.5-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 	"unsafe"
 	"github.com/supermuesli/computeshader/pkg/shaders"
+	"github.com/supermuesli/computeshader/internal/shaderutils"
 )
 
-const windowWidth = 800
-const windowHeight = 600
+const (
+	windowWidth = 800
+	windowHeight = 600
+)
 
-// blabal the glaglagu
 func init() {
 	// glfw event handling must run on the main OS thread
 	runtime.LockOSThread()
@@ -81,13 +82,13 @@ func main() {
 	fmt.Println("***-----------------------------------------------------------------------------***")
 
 	// configure compute shader
-	computeShader, err := newComputeShader(shaders.computeSrc)
+	computeShader, err := shaderutils.NewComputeShader(shaders.ComputeSrc)
 	if err != nil {
 		panic(err)
 	}
 
 	// configure fullscreen quad shader
-	quadShader, err := newQuadShader(shaders.vertexSrc, shaders.FragmentSrc)
+	quadShader, err := shaderutils.NewQuadShader(shaders.VertexSrc, shaders.FragmentSrc)
 	if err != nil {
 		panic(err)
 	}
@@ -162,91 +163,4 @@ func main() {
 		
 		window.SwapBuffers()
 	}
-}
-
-func newComputeShader(computeShaderSource string) (uint32, error) {
-	computeShader, err := compile(computeShaderSource, gl.COMPUTE_SHADER)
-	if err != nil {
-		return 0, err
-	}
-
-	program := gl.CreateProgram()
-
-	gl.AttachShader(program, computeShader)
-	gl.LinkProgram(program)
-
-	var status int32
-	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to link program: %v", log)
-	}
-
-	gl.DeleteShader(computeShader)
-
-	return program, nil
-}
-
-func newQuadShader(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
-	vertexShader, err := compile(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		return 0, err
-	}
-
-	fragmentShader, err := compile(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		return 0, err
-	}
-
-	program := gl.CreateProgram()
-
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.BindAttribLocation(program, 0, gl.Str("pos\x00"))
-	gl.LinkProgram(program)
-
-	var status int32
-	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to link program: %v", log)
-	}
-
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
-
-	return program, nil
-}
-
-func compile(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csources, free := gl.Strs(source)
-	gl.ShaderSource(shader, 1, csources, nil)
-	free()
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
 }
---
+++ internal/shaderutils/shaderutils.go
@@ -0,0 +1,94 @@
+package shaderutils
+
+import (
+	"github.com/go-gl/gl/v4.5-core/gl"
+	"strings"
+	"fmt"
+)
+
+func compile(source string, shaderType uint32) (uint32, error) {
+	shader := gl.CreateShader(shaderType)
+
+	csources, free := gl.Strs(source)
+	gl.ShaderSource(shader, 1, csources, nil)
+	free()
+	gl.CompileShader(shader)
+
+	var status int32
+	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
+	}
+
+	return shader, nil
+}
+
+func NewComputeShader(computeShaderSource string) (uint32, error) {
+	computeShader, err := compile(computeShaderSource, gl.COMPUTE_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, computeShader)
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(computeShader)
+
+	return program, nil
+}
+
+func NewQuadShader(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
+	vertexShader, err := compile(vertexShaderSource, gl.VERTEX_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	fragmentShader, err := compile(fragmentShaderSource, gl.FRAGMENT_SHADER)
+	if err != nil {
+		return 0, err
+	}
+
+	program := gl.CreateProgram()
+
+	gl.AttachShader(program, vertexShader)
+	gl.AttachShader(program, fragmentShader)
+	gl.BindAttribLocation(program, 0, gl.Str("pos\x00"))
+	gl.LinkProgram(program)
+
+	var status int32
+	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
+	if status == gl.FALSE {
+		var logLength int32
+		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
+
+		log := strings.Repeat("\x00", int(logLength+1))
+		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
+
+		return 0, fmt.Errorf("failed to link program: %v", log)
+	}
+
+	gl.DeleteShader(vertexShader)
+	gl.DeleteShader(fragmentShader)
+
+	return program, nil
+}
